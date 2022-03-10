1184b4a1351c5ff43f80635c5b5b1138cb4a908d
borashek32 <borashek@inbox.ru>
2022-03-10 19:48:54 +0300

    registration and login tests dusk

--- tests/Browser/User/Auth/LoginTest.php
+++ tests/Browser/User/Auth/LoginTest.php
@@ -3,18 +3,20 @@
 namespace Tests\Browser\User\Auth;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Laravel\Dusk\Browser;
 use App\Models\User;
 use Tests\DuskTestCase;
 
 class LoginTest extends DuskTestCase
 {
-    public function setUp(): void
-    {
-        parent::setUp();
+    // public function setUp(): void
+    // {
+    //     parent::setUp();
 
-        $user = User::factory()->create();
-    }
+    //     $user = User::factory()->create();
+    // }
 
     public function test_user_can_not_login_with_wrong_credentials()
     {
@@ -34,18 +36,29 @@ class LoginTest extends DuskTestCase
 
     public function test_user_can_login_with_correct_credentials()
     {
-        $user_email = User::OrderBy('id', 'DESC')->first()->email;
-        $user_name  = User::OrderBy('id', 'DESC')->first()->name;
+        $user_id       = random_int(1, 12);
+        $user_email    = User::where('id', $user_id)->first()->email;
+        $user_name     = User::where('id', $user_id)->first()->name;
         
         $this->browse(function (Browser $browser) use($user_name, $user_email) {
             $browser->visit('/login')
                     ->type('#email', $user_email)
                     ->type('#password', 'password')
-                    ->assertSee('Электронная почта')
                     ->press('ВОЙТИ')
-                    ->assertSee('Добро пожаловать')
                     ->assertPathIs('/dashboard')
                     ->assertSee($user_name);
+        });
+    }
+
+    public function test_user_can_logout()
+    {
+        $user_id = random_int(1, 12);
+        
+        $this->browse(function ($browser) use($user_id) {
+            $browser->loginAs(User::find($user_id))
+                    ->visit('/logout')
+                    ->logout()
+                    ->assertGuest();
         });
     }
 }
--- tests/Browser/User/Auth/RegistrationTest.php
+++ tests/Browser/User/Auth/RegistrationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Browser\User\Auth;
 
 // use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
@@ -31,19 +32,18 @@ class RegistrationTest extends DuskTestCase
         });
     }
 
-    public function test_user_can_not_register_with_valid_credentials()
+    public function test_user_can_register_with_valid_credentials()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/register')
                     ->type('#name', 'Polina')
                     ->type('#email', Str::random(4) . '@' . Str::random(3) . '.' . Str::random(2))
-                    ->type('#phone', rand(9, 10))
+                    ->type('#phone', rand(10000000, 1010000000))
                     ->type('#password', '00000000')
                     ->type('#password_confirmation', '00000000')
                     ->assertSee('Электронная почта')
                     ->check('#status', '1')
                     ->press('РЕГИСТРАЦИЯ')
-                    ->pause(1000)
                     ->assertPathIs('/dashboard');
         });
     }
